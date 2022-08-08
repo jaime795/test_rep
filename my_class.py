3d2bb8fd0906ac4b4748664326e2d95152af341b
Jaime Rodríguez <jaime.r.m.795@gmail.com>
2022-08-08 16:16:27 +0200

    Create Class Square with attributes and methods

--- my_class.py
+++ my_class.py
@@ -1,8 +1,27 @@
-from pyclbr import Class
-import pandas as pd
-import numpy
+class Square:
+    def __init__(self, side):
+        self.side = side
+    
+    def area(self):
+        return self.side**2
 
-lista=[1,2,3,4]
+    def perimeter(self):
+        return self.side*4
+    
+    def __repr__(self):
+        #Declares how a Square object should be printed
+        #str() computes the informal string representation of an object
+        #repr() compute the official string representation of an object, use for debugging
+        s = 'Sqaure with side = ' + str(self.side) + '\n' + "Area = " + str(self.area()) + "\n" + "Perimeter = " + str(self.perimeter())
+        return s
 
-for i in range (4):
-    print(i)
+if __name__ == '__main__':
+    #Read from user
+    side = eval(input('Enter the side length to create a Square: '))
+
+    #Create a square with that side
+    square = Square(side)
+
+    #print the created square
+    print(square)
+        
